9a4d823076019b59301d8f9413ff4efa49a397c2
santhosh <12345678+yourusername@users.noreply.github.com>
2026-06-13 13:47:19 +0530

    vowel or nor

---
+++ Vowel_or_Not.py
@@ -0,0 +1,5 @@
+l=str(input())
+if l=='a' or l=='e' or l=='i' or l=='o' or l=='u' or l=='A' or l=='E' or l=='I' or l=='O' or l=='U':
+    print('{} is vowel'.format(l))
+else:
+    print('{} is consonant'.format(l))
